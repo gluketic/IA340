cbc77a176f28584d072c1ada9605b025d533abcb
gluketic <luketigt@dukes.jmu.edu>
2024-09-16 17:51:35 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,5 @@
-
+4.1 
+delete from public.name
+where fips = 'VA'
+4.2 
+The code "insert into income(fips,income,year) values('80',6000,2025)" did not work because it says that fips = "80" is not present in the table name
